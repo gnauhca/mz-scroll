4b596247106b1a0cdcb6350fe4ae2bc1efc82e74
zhzhchwin <576073927@qq.com>
2015-12-06 22:28:22 +0800

    first commit!

---
+++ l-scroll.js
@@ -0,0 +1,143 @@
+var Scroller = function() {
+	var scrollTasks = [],
+		defaults = {
+			once: true,
+			delay: 0,
+			inScreen: 0.5,
+			topOffset: 0,
+			bottomOffset: 0,
+			activeClass: '',
+			scrollIn: null,
+			scrollOut: null,
+			scrolling: null
+		};
+
+	//初始化，监听window scroll事件
+	function init() {
+		var $window = $(window),
+			lastScrollTop = $window.scrollTop(),
+			scrollOffset = 0;
+
+		$(window).on('scroll', function() {
+			scrollOffset = $window.scrollTop() - lastScrollTop;
+			lastScrollTop = $window.scrollTop();
+			fireTasks(scrollOffset);
+		});
+	}
+
+	function fireTasks(scrollOffset) {
+		var task,
+			inScreen,
+			sizeInfo,
+			actived;
+
+		//debug.innerHTML = '';
+		for (var i = 0, l=scrollTasks.length; i < l; i++) {
+			task = scrollTasks[i];
+			sizeInfo = getSizeInfo(task.elem, task.options.topOffset, task.options.bottomOffset);
+
+
+			if (sizeInfo.scrollIn) {
+				actived = true;
+				if (task.actived !== actived) {
+					setTimeout((function(task) {
+						return function() {
+							if (!task.actived) return;
+							$(task.elem).addClass(task.options.activeClass)
+							task.options.scrollIn && task.options.scrollIn.call(task.elem);
+						}
+					})(task), task.options.delay);						
+				}
+				task.options.scrolling && task.options.scrolling.call(task.elem, scrollOffset);
+				if (task.options.once) {
+					scrollTasks.splice(i, 1);
+				}
+				task.actived = actived;
+			} else if (sizeInfo.scrollOut){
+				actived = false;
+				if (task.actived !== actived) {
+					task.options.scrollOut && task.options.scrollOut.call(task.elem);
+					$(task.elem).removeClass(task.options.activeClass);
+					task.actived = actived;
+				}
+			}
+			//debug.innerHTML += '<br> ' + task.elem.id + ': ' + task.actived + '<br />';
+		}
+	}
+
+
+	function getSizeInfo(elem, topOffset, bottomOffset) {
+		var elemHeight = elem.offsetHeight,
+			elemOfsTop = $(elem).offset().top,
+			windowScrollTop = $(window).scrollTop(),
+			windowHeight = $(window).height(),
+			sizeInfo = {/*scrollOut, scrollIn*/};	
+
+		//判断scrollIn时机，注意scrollIn，scrollOut时机并不是互斥的
+		if (elemOfsTop < windowScrollTop + windowHeight + topOffset &&
+			elemOfsTop + elemHeight > windowScrollTop - bottomOffset) {
+			sizeInfo.scrollIn = true;
+		} else {
+			sizeInfo.scrollIn = false;
+		}
+
+		//判断scrollOut时机
+		if (elemOfsTop > windowScrollTop + windowHeight + (bottomOffset < 0 ? 0: bottomOffset) ||
+			elemOfsTop + elemHeight < windowScrollTop + (topOffset > 0 ? 0 : topOffset)) {
+			sizeInfo.scrollOut = true;
+		} else {
+			sizeInfo.scrollOut = false;
+		}
+		return sizeInfo;
+	}
+
+	this.addTask = function(elem, taskOptions) {
+		var taskOptions = $.extend(true, defaults, taskOptions),
+			elem = elem;
+
+		scrollTasks.push({
+			'elem': elem,
+			'actived': false,
+			'options': taskOptions
+		});
+		fireTasks(0);
+	}
+
+	this.removeTask = function(elem) {
+		for (var i = scrollTasks.length - 1; i >= 0; i--) {
+			if (scrollTasks[i].elem === elem) {
+				scrollTasks.splice(i, 1);
+			}
+		};
+	}
+	init();
+}
+
+/*
+ * options 配置 如果传false则删除此元素监听任务
+ *
+ * once {boolean} 是否执行一次 default false
+ * delay {int} 元素进入区域延时
+ * topOffset {int} 元素顶部到窗口底部的距离多少算进入区域 default 0
+ * bottomOffset {int} 元素底部到窗口顶部部的距离多少算进入区域 default 0
+ * activeClass {string} 元素进入区域要添加的类
+ * scrollIn {function} 元素进入区域的回调
+ * scrollOut {function} 元素离开区域的回调
+ * scrolling 
+ * 
+ */
+$.fn.addScroll = (function() {
+	var scroller = new Scroller();
+
+	return function(options) {
+
+		this.each(function() {
+			if (options === false) {
+				scroller.removeTask(this);
+			} else {
+				scroller.addTask(this, options);
+			}
+		});
+		return this;
+	}
+})()
---
+++ readme.md
@@ -0,0 +1,23 @@
+# l-scroller jquery 插件
+
+插件用于简化元素监听scroll事件的方法，当你页面中又一个元素在滚动到某个位置的时候需要触发一些动作的时候。比如图片的延时加载，滚动出现播放css动画等等。
+
+使用方法
+
+
+	$(selector).addScroll(options);
+
+
+ 
+options {obj} 配置 如果传false则删除此元素监听任务
+ 
+---
+
+ * 具体配置选项：
+ * once {boolean} 是否执行一次 default false
+ * delay {int} 元素进入区域延时
+ * topOffset {int} 元素顶部到窗口底部的距离多少算进入区域 default 0
+ * bottomOffset {int} 元素底部到窗口顶部部的距离多少算进入区域 default 0
+ * activeClass {string} 元素进入区域要添加的类
+ * scrollIn {function} 元素进入区域的回调
+ * scrollOut {function} 元素离开区域的回调
